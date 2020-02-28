71b1833f11c7aa34202415c866d318a77898fb63
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-02-28 11:38:24 -0600

    auth_code expiring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
   end
   
   def show
-    user_id = request.original_url.last(1)
-    user = User.where(id: user_id)
-    ac = user.auth_code
+    # user_id = request.original_url.last(1)
+    # user = User.where(id: user_id)
+    # ac = user.auth_code
   end
 
   def create
