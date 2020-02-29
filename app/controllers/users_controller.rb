8d34469377cf8374d1590b01b567ce75b91bf704
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-02-28 23:06:02 -0600

    testing faraday

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
 
-  # require 'faraday'
-
   @@app_landing = URI.escape('https://floating-hamlet-63269.herokuapp.com/create', Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
   @@spotify_client_id = 'd7f2ddcb58784a428ff86348869cbfd9'
   @@spotify_client_secret = '164b375ae4864c11a25810f923ebf9c8'
@@ -18,37 +16,17 @@ class UsersController < ApplicationController
   end
 
   def create
-
     auth_code = request.original_url.last(352)
-    request = Faraday.new
-    access_token_json = request.post do |req|
-      req.url 'https://accounts.spotify.com/api/token'
-      req.headers['Authorization'] = 'Basic'
-      req.body = URI.encode_www_form(
+    access_token_json = Faraday.post(
+      'https://accounts.spotify.com/api/token',
+      URI.encode_www_form(
         client_id: @@spotify_client_id,
         client_secret: @@spotify_client_secret,
         grant_type: 'authorization_code',
         code: auth_code,
         redirect_uri: @@app_landing
       )
-    end
-
-
-
-
-
-
-    
-    # access_token_json = Faraday.new(
-    #   'https://accounts.spotify.com/api/token',
-    #   headers: { 'Authorization' => 'Basic' },
-    #   URI.encode_www_form(
-    #     client_id: @@spotify_client_id,
-    #     client_secret: @@spotify_client_secret,
-    #     grant_type: 'authorization_code',
-    #     code: auth_code,
-    #     redirect_uri: @@app_landing
-    #   )).post
+    )
     @user = User.create(auth_code: auth_code, access_token_json: access_token_json)
     redirect_to(user_path(@user))
   end
