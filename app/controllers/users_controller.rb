da501d9a7d908ee0c57395a487a7b2241bad09d3
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-02-28 20:34:19 -0600

    debugging httparty json to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     auth_code = request.original_url.last(352)
     # auth_code = "AQAWPSfcDXNTWOhnEDKoB9ny0_ee1NYSHpfNiOTJYzjAOUI2MbI39IwH39T2BYsegEnmiENpgvw7UKDqJnGqZEcSrLPHTPnIXKYKFqLpuu4Kn5gCL3cuGAD7Uwyc2OZBcqcKs5BNuuPFk2n6Acfth9Lu3td1ELzyqXHpKgRNHr7NEhjENhCDzqS0neMaCYlapJJXxLs_DALCPC2jLAPdDsBIqzRJcWTEPapXIw5_c0JtO9cRdt8uj90IsDvwFPxHDRrWqEOu7bzQOUo72bOW7pTRPerRy3Nf7md0VtIqgd8ZAPbNd9ZtOjHipekMDykieJNjsETY6liWeOsvUSfGE6amTzqIEU0v"
     access_token_json = HTTParty.post("https://accounts.spotify.com/api/token",
-      body: { client_id: @spotify_client_id,
+      body: { client_id: @@spotify_client_id,
               client_secret: '164b375ae4864c11a25810f923ebf9c8',
               grant_type: 'authorization_code',
               code: auth_code,
