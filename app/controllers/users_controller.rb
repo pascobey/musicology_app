a99f56010c56cc8a4d19ada58afcb7efc0784908
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-02-29 00:56:21 -0600

    testing Faraday

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,16 +17,17 @@ class UsersController < ApplicationController
 
   def create
     auth_code = request.original_url.last(352)
-    access_token_json = Faraday.post(
+    access_token_json = Faraday.new(
       'https://accounts.spotify.com/api/token',
-      headers: {
-        'Authorization: Basic ZDdmMmRkY2I1ODc4NGE0MjhmZjg2MzQ4ODY5Y2JmZDk6MTY0YjM3NWFlNDg2NGMxMWEyNTgxMGY5MjNlYmY5Yzg='
-      },
-      parameters: {
-        "grant_type" => "authorization_code", 
-        "code" => "#{auth_code}",
-        "redirect_uri" => "#{@@app_landing}"
-      })
+      URI.encode_www_form(
+        grant_type: "authorization_code",
+        code: auth_code,
+        redirect_uri: @@app_landing
+      ),
+      {
+        'Authorization: Basic', 'ZDdmMmRkY2I1ODc4NGE0MjhmZjg2MzQ4ODY5Y2JmZDk6MTY0YjM3NWFlNDg2NGMxMWEyNTgxMGY5MjNlYmY5Yzg='
+      }
+    )
     # access_token_json = Faraday.post(
     #   'https://accounts.spotify.com/api/token',
     #   URI.encode_www_form(
