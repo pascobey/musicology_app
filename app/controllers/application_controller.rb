d51ff4b960617d7b3eb1386a9013b49f670713a5
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-03-01 09:41:04 -0600

    Base64.encode not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     APP_LANDING_URI = URI.escape(APP_LANDING_URL, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")).freeze
     APP_CLIENT_ID = 'd7f2ddcb58784a428ff86348869cbfd9'.freeze
     APP_CLIENT_SECRET = '164b375ae4864c11a25810f923ebf9c8'.freeze
-    CLIENT_B64 = Base64.encode64("#{APP_CLIENT_ID}:#{APP_CLIENT_SECRET}").freeze
+    CLIENT_B64 = "ZDdmMmRkY2I1ODc4NGE0MjhmZjg2MzQ4ODY5Y2JmZDk6MTY0YjM3NWFlNDg2NGMxMWEyNTgxMGY5MjNlYmY5Yzg=".freeze
     SCOPES = 'user-top-read user-follow-read user-library-read user-read-recently-played user-read-email'.freeze
     SCOPES_URI = URI.escape(SCOPES, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")).freeze
 end
