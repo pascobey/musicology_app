b4ace93fdea6775697eb81aae411e855cdccdd11
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-03-01 16:39:40 -0600

    get request?

--- db/migrate/20200227002302_create_users.rb
+++ db/migrate/20200227002302_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def up
     create_table :users do |t|
+      t.json :user_profile_json
       t.string :refresh_token
       t.timestamps
     end
