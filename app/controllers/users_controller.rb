809dc785e9e493428d56ead8d4602de7d5327bb5
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-03-02 15:32:54 -0600

    dying and debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
           Authorization: "Bearer #{access_token_json['access_token']}"
         }
       )
-      puts playlist_tracks_json['items']
+      puts playlist_tracks_json['items']['track']
     end
     redirect_to(user_path(@user))
   end
