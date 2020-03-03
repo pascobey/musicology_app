17c296b0aa01e1fef9e228d781a1ac75fecebe72
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-03-03 16:03:51 -0600

    dubugging flash w jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,8 +14,9 @@ require('jquery')
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
 $(document).ready(function() {
     $('#flash').fadeOut("normal", function() {
         $(this).remove();
