f2b87bbb1e3eb3419091e67c9741afe0633f924e
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-03-03 16:01:59 -0600

    dubugging flash w jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,13 +13,11 @@ require('jquery')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-// $(document).ready(function() {
-//     setTimeout(function() {
-//         $('#flash').fadeOut("normal", function() {
-//             $(this).remove();
-//         });
-//     }, 2000);
-// })
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+$(document).ready(function() {
+    $('#flash').fadeOut("normal", function() {
+        $(this).remove();
+    });
+})
