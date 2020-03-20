74b0c4ee057718c95cf0c0e80b638f41af283a3e
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-03-20 14:13:04 -0500

    beginning to build/debug user.library update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+
+
   def new
     if User.find_by(user_id: cookies[:user_id])
       flash[:notice] = "USER FOUND! Welcome Back, #{User.find_by(user_id: cookies[:user_id]).email}"
@@ -11,6 +13,8 @@ class UsersController < ApplicationController
       '&scope=' + SCOPES_URI
   end
   
+
+
   def show
     @user = User.find_by(id: request.original_url[("#{APP_BASE_URL}/users/").size, 100000000])
     if !@user || @user.user_id != cookies[:user_id]
@@ -18,6 +22,8 @@ class UsersController < ApplicationController
       redirect_to('/')
     end
   end
+
+
 
   def create
     auth_code = request.original_url.last(352)
@@ -34,43 +40,49 @@ class UsersController < ApplicationController
         Authorization: "Bearer #{access_token_json['access_token']}"
       }
     )
-    @user = User.create(user_id: user_profile_json['id'], email: user_profile_json['email'],
-      account_url: user_profile_json.dig('external_urls', 'spotify'), refresh_token: access_token_json['refresh_token'])
-    @library = Library.create(user_id: @user.id)
-    playlists_json = HTTParty.get(
-      "#{SPOTIFY_API_URL}/v1/me/playlists",
-      headers: {
-        Authorization: "Bearer #{access_token_json['access_token']}"
-      }
-    )
-    playlists_json['items'].each do |p|
-      Playlist.create(library_id: @library.id, spotify_unique: p['id'], name: p['name'])
-    end
-    playlists = Playlist.where(library_id: @library.id)
-    playlists.each do |p|
-      playlist_tracks_json = HTTParty.get(
-        "#{SPOTIFY_API_URL}/v1/playlists/#{p.spotify_unique}/tracks",
+    if !User.find_by(user_id: user_profile_json['id'])
+      @user = User.create(user_id: user_profile_json['id'], email: user_profile_json['email'],
+        account_url: user_profile_json.dig('external_urls', 'spotify'), refresh_token: access_token_json['refresh_token'])
+      @library = Library.create(user_id: @user.id)
+      playlists_json = HTTParty.get(
+        "#{SPOTIFY_API_URL}/v1/me/playlists",
         headers: {
           Authorization: "Bearer #{access_token_json['access_token']}"
         }
-      )['items']
-      playlist_tracks_json.each do |t|
-        artists_names = ''
-        artists_hash = t['track']['artists']
-        artists_hash.each do |ah|
-          if ah != artists_hash.first
-            artists_names += ', '
-          end
-          if !Artist.find_by(library_id: @library.id, name: ah['name'])
-            Artist.create(library_id: @library.id, name: ah['name'])
-          end
-          artists_names += ah['name']
-        end
-        Track.create(playlist_id: p.id, artists_names: artists_names, track_name: t['track']['name'], album_name: t['track']['album']['name'])      
+      )
+      playlists_json['items'].each do |p|
+        Playlist.create(library_id: @library.id, spotify_unique: p['id'], name: p['name'])
       end
+      playlists = Playlist.where(library_id: @library.id)
+      playlists.each do |p|
+        playlist_tracks_json = HTTParty.get(
+          "#{SPOTIFY_API_URL}/v1/playlists/#{p.spotify_unique}/tracks",
+          headers: {
+            Authorization: "Bearer #{access_token_json['access_token']}"
+          }
+        )['items']
+        playlist_tracks_json.each do |t|
+          artists_names = ''
+          artists_hash = t['track']['artists']
+          artists_hash.each do |ah|
+            if ah != artists_hash.first
+              artists_names += ', '
+            end
+            if !Artist.find_by(library_id: @library.id, name: ah['name'])
+              Artist.create(library_id: @library.id, name: ah['name'])
+            end
+            artists_names += ah['name']
+          end
+          Track.create(playlist_id: p.id, artists_names: artists_names, track_name: t['track']['name'], album_name: t['track']['album']['name'])      
+        end
+      end
+      cookies.permanent[:user_id] = @user.user_id
+    else
+      # UPDATE INFORMATION
     end
-    cookies.permanent[:user_id] = @user.user_id
     redirect_to(user_path(@user))
   end
 
+
+  
 end
