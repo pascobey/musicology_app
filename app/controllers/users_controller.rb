bd726c3acb2033f0cbf4781ed7d83c0d743db3de
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-04-16 12:42:50 -0500

    moving application_controller variables to user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 
-
+  SPOTIFY_BASE_URL = 'https://accounts.spotify.com'
+  SPOTIFY_API_URL = 'https://api.spotify.com'
+  APP_BASE_URL = 'https://graphurmusic.com'
+  APP_LANDING_URL = "#{APP_BASE_URL}/create"
+  APP_LANDING_URI = URI.escape(APP_LANDING_URL, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  APP_CLIENT_ID = 'd7f2ddcb58784a428ff86348869cbfd9'
+  APP_CLIENT_SECRET = '164b375ae4864c11a25810f923ebf9c8'
+  CLIENT_B64 = "ZDdmMmRkY2I1ODc4NGE0MjhmZjg2MzQ4ODY5Y2JmZDk6MTY0YjM3NWFlNDg2NGMxMWEyNTgxMGY5MjNlYmY5Yzg="
+  SCOPES = 'user-top-read user-follow-read user-library-read user-read-recently-played user-read-email'
+  SCOPES_URI = URI.escape(SCOPES, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
 
   def new
     if User.find_by(user_id: cookies[:user_id])
