389c93eed9aa25b900b66e4f538e737003164a25
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-04-16 13:36:45 -0500

    fixed typo in APP_LANDING_URI init/assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     SPOTIFY_API_URL = 'https://api.spotify.com'
     APP_BASE_URL = 'https://www.graphurmusic.com'
     APP_LANDING_URL = "#{APP_BASE_URL}/create"
-    APP_LANDING_URI = URI.escape(APP_BASE_URL, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    APP_LANDING_URI = URI.escape(APP_LANDING_URL, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
     APP_CLIENT_ID = 'd7f2ddcb58784a428ff86348869cbfd9'
     APP_CLIENT_SECRET = '164b375ae4864c11a25810f923ebf9c8'
     CLIENT_B64 = "ZDdmMmRkY2I1ODc4NGE0MjhmZjg2MzQ4ODY5Y2JmZDk6MTY0YjM3NWFlNDg2NGMxMWEyNTgxMGY5MjNlYmY5Yzg="
