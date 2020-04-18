688e58c14f21cdce3f8722a585e6801254f02d6a
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-04-17 21:21:53 -0500

    debugging track create

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -71,7 +71,7 @@ class TracksController < ApplicationController
           puts main_artist_unique = Artist.find_by(name: main_artist_name).artist_spotify_unique
           puts p_id = p.id
           puts "create track..."
-          puts Track.create(playlist_id: p_id, artist_spotify_unique: main_artist_unique, artists_names: artists_names, track_name: t['track']['name'], album_name: t['track']['album']['name'])   
+          # puts Track.create(playlist_id: p_id, artist_spotify_unique: main_artist_unique, artists_names: artists_names, track_name: t['track']['name'], album_name: t['track']['album']['name'])   
           puts "track created?"   
         end
       end
