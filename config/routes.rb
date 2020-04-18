6b2e6468c709556a76dc1d7d5beef8f2005a5220
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-04-17 22:30:55 -0500

    debugging track create

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'tracks/create'
+  get 'tracks/show'
+  get 'playlists/create'
+  get 'playlists/show'
   root 'users#new'
   get 'create', to: 'users#create'
   resources :users, only: [:show]
-  resources :tracks, only: [:show, :create]
-  resources :playlists, only: [:show, :create]
 
 end
