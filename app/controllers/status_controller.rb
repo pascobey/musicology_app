fd2445b1f6b45040a2a67ff348b7c4af49397efc
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-04-20 11:28:43 -0500

    adding - status controller, finished finish_build/update_build actions after playlist gather to notify user of long wait time

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -4,10 +4,10 @@ class StatusController < ApplicationController
     puts "started finished"
     url_vars = retrieve_url_vars(request.original_url)
     library = Library.find_by(id: url_vars[:library_id])
-    if !library.playlist.find_by(id: library.playlists.size).tracks
+    if !library.playlists.find_by(id: library.playlists.size).tracks
       puts "should redirect to finish_build"
       redirect_to controller: 'status', action: 'finish_build', library_id: url_vars[:library_id], access_token: url_vars[:access_token]
-    else library.playlist.find_by(id: 1).tracks
+    else library.playlists.find_by(id: 1).tracks
       puts "should redirect to update_build"
       redirect_to controller: 'status', action: 'update_build', library_id: url_vars[:library_id], access_token: url_vars[:access_token]
     end
