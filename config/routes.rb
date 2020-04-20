157febf0893ae9dbface845d9bae1a9161e7cdee
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-04-20 10:49:04 -0500

    debugging - not retrieving all playlists - adding query limit: 50 --- adding - finish_build/update_build after playlist gather to notify user of long wait time

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   get 'tracks/create'
+  get 'tracks/update'
   get 'playlists/create'
+  get "playlists/update"
   root 'users#new'
   get 'create', to: 'users#create'
-  resources :users, only: [:show, :finish_build, :update_build]
+  resources :users, only: [:show, :finished, :finish_build, :update_build]
 
 end
