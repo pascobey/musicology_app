66ff901c5235991fce4606224d79d661b1c1a81c
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-04-23 12:51:39 -0500

    debug - javascript omit webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,13 +16,13 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-window.onload = autoClick();
-function autoClick() {
-    if (document.getElementById("build-content-container")) {
-        if (document.getElementById("js-click").innerHTML) {
-            setTimeout(function() {
-                document.getElementById(document.getElementById("js-click").innerHTML).click();
-            }, 1200);
-        }
-    }
-}
+// window.onload = autoClick();
+// function autoClick() {
+//     if (document.getElementById("build-content-container")) {
+//         if (document.getElementById("js-click").innerHTML) {
+//             setTimeout(function() {
+//                 document.getElementById(document.getElementById("js-click").innerHTML).click();
+//             }, 1200);
+//         }
+//     }
+// }
