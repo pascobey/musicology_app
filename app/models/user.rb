4b5acf8dfea60807a54063ec0c46f956f9e0c2c7
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-04-23 22:49:44 -0500

    stratified playlists, move to stratify library

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
         playlist = Playlist.find_by(spotify_unique: playlist_spotify_unique)
         playlist.tracks.each do |t|
             artists_names_array = []
-            puts artists_names_string = t.artists_names
+            artists_names_string = t.artists_names
             if artists_names_string.include?("|")
                 until artists_names_string == ""
                     if artists_names_string.include?("|")
@@ -22,7 +22,6 @@ class User < ApplicationRecord
                 artists_names_array << artists_names_string
             end
             artists_names_array.each do |a|
-                puts a
                 temp_artists_array << Artist.find_by(name: a).artist_spotify_unique
             end
         end
