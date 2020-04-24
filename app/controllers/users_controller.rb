4afa80295ff562a311df5c535d653030f7c24249
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2020-04-24 16:14:02 -0500

    stratified playlists, move to stratify library

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 
-  before_action :authorized?, except: [:new, :create]
 
   def new
     if User.find_by(user_id: cookies[:user_id])
@@ -15,15 +14,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: request.original_url.gsub("#{APP_BASE_URL}/users/", ""))
-    if !@user
-      flash[:notice] = "Not permitted, please sign in."
-      redirect_to('/')
-    else @user.user_id != cookies[:user_id]
-      redirect_to(user_path(User.find_by(user_id: cookies[:user_id]).id))
-    end
+    # if !@user
+    #   flash[:notice] = "Not permitted, please sign in."
+    #   redirect_to('/')
+    # else @user.user_id != cookies[:user_id]
+    #   redirect_to(user_path(User.find_by(user_id: cookies[:user_id]).id))
+    # end
     @playlists = Library.find_by(user_id: request.original_url.gsub("#{APP_BASE_URL}/users/", "")).playlists
     @playlists_stratifications = []
     @playlists.each do |p|
+      puts p.id
       @playlists_stratifications << User.stratify_artist_representation_in_playlist(p.spotify_unique)
     end
   end
