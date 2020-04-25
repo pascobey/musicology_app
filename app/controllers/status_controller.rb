10cc04254934e85d589a872eee8d75f837b40143
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-04-24 20:17:12 -0500

    debug - status controller actions

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -7,7 +7,7 @@ class StatusController < ApplicationController
     @library_id = url_vars[:library_id]
     @access_token = url_vars[:access_token]
     @playlists = Playlist.where(library_id: @library_id)
-    puts @playlists
+    puts @playlists.find_by(id: @playlists.size)
     @playlist_id = url_vars[:playlist_id]
     # Conditional checks if every playlist empty or are there new playlists?
     if @playlists.find_by(id: @playlists.size).tracks == []
