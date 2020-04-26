758dda83ecf9df3c8dac2301b13e05759b93a93c
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-04-25 22:30:22 -0500

    designing build K graphs

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,6 +2,7 @@ class Playlist < ApplicationRecord
 
     belongs_to :library
     has_many :tracks
+    has_many :artists, :through => :tracks
 
     def self.retrieve_playlists_json(spotify_api_url, access_token)
         playlists_json = HTTParty.get(
